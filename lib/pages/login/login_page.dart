283ec57d1cc39eb310217b3e64b14eec91705c61
eunice <ngoyaieunice@gmail.com>
2024-12-31 10:36:53 +0300

    formatted the code

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,4 +1,6 @@
 
+// ignore_for_file: avoid_print
+
 import 'dart:convert'; // For encoding and decoding JSON
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http; // For making HTTP requests
